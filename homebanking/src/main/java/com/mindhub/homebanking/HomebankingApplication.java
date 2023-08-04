e7d5c91a3eebd30e57800c60a66fd17be33ff67e
LuisVazquez3000 <luis.vazquez.r25@gmail.com>
2023-08-04 02:11:27 -0300

    first commit

---
+++ homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -0,0 +1,41 @@
+package com.mindhub.homebanking;
+
+import com.mindhub.homebanking.Models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+@Bean
+public CommandLineRunner initData(ClientRepository clientRepository){
+
+return (args -> {
+Client client1 = new Client();
+		client1.setId(123);
+		client1.setFirstName("Luis");
+		client1.setLastName("Vazquez");
+		client1.setEmail("luis@vazquez.com");
+
+Client client2 = new Client();
+		client2.setId(321);
+		client2.setFirstName("Martin");
+		client2.setLastName("Cruz");
+		client2.setEmail("martin@cruz.com");
+
+
+		clientRepository.save(client1);
+		clientRepository.save(client2);
+});
+
+}
+
+
+}
---
+++ homebanking/src/main/java/com/mindhub/homebanking/Models/Client.java
@@ -0,0 +1,78 @@
+package com.mindhub.homebanking.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    public Client() {
+    }
+
+    public Client(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Client(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public  int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
